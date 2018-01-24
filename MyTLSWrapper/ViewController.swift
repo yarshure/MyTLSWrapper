bd1cb89f936e3e6131f6a19828a83bae67bcd582
yarshure <yarshure@gmail.com>
2018-01-24 09:51:36 +0800

    Initial Commit

---
+++ MyTLSWrapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyTLSWrapper
+//
+//  Created by yarshure on 2018/1/24.
+//  Copyright © 2018年 yarshure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
